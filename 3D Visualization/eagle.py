078662d43682c88e6e96f23d5b3aa9fce2f51e48
DuruOz <oyaduruoz@gmail.com>
2026-04-16 11:45:29 +0200

    Added the data files to Data folder

--- 3D Visualization/eagle.py
+++ 3D Visualization/eagle.py
@@ -3,31 +3,20 @@ import pyvista as pv
 df = pd.read_csv("Data/processed_acc_day1.csv")
 print(df.columns.tolist())
 print(df.head())
+
 plotter = pv.Plotter(window_size=[1000, 700])
 floor = pv.Plane(center=(0, 0, 0), direction=(0, 0, 1), i_size=10, j_size=10)
 plotter.add_mesh(floor, opacity=0.5)
-# -------------------------
-# ADD PLACEHOLDER "BIRD"
-# -------------------------
-bird = pv.Cube(center=(0, 0, 1),
-               x_length=1.5,
-               y_length=0.4,
-               z_length=0.2)
+
+bird = pv.Cube(center=(0, 0, 1), x_length=1.5, y_length=0.4, z_length=0.2)
 
 plotter.add_mesh(bird, show_edges=True)
-
-# -------------------------
-# ADD AXES (important for orientation)
-# -------------------------
-plotter.add_axes()
-plotter.show_grid()
 
 # -------------------------
 # TEMPORARY LEGEND VALUES
 # (we will replace with real data)
 # -------------------------
 row = df.iloc[0]  # first row for now
-
 
 time_val = row.get("time", 0)
 roll_val = row.get("roll", 0)
@@ -47,9 +36,6 @@ plotter.add_text(legend_text,
                  position="upper_left",
                  font_size=12)
 
-# -------------------------
-# SHOW WINDOW
-# -------------------------
 plotter.show()
 
 
